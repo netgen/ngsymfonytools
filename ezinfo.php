6b7ce7baa75f54a5e51fa1fe6792bd2d71a22fb6
Edi Modrić <edi@netgen.hr>
2014-02-06 09:09:51 +0000

    Add ezinfo.php

---
+++ ezinfo.php
@@ -0,0 +1,14 @@
+<?php
+
+class ngsymfonytoolsInfo
+{
+    static public function info()
+    {
+        return array(
+            'Name' => '<a href="http://projects.ez.no/ngsymfonytools">Netgen Symfony Tools</a> extension',
+            'Version' => '1.0',
+            'Copyright' => 'Copyright (C) 2014 Netgen d.o.o.',
+            'License' => 'GNU General Public License v2.0'
+        );
+    }
+}
