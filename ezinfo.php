16e8addfa02a6e225f3d8077bcefada2f267fdd3
Edi Modrić <edi.modric@gmail.com>
2014-11-08 17:57:09 +0100

    Update version

--- ezinfo.php
+++ ezinfo.php
@@ -6,7 +6,7 @@ class ngsymfonytoolsInfo
     {
         return array(
             'Name' => '<a href="http://projects.ez.no/ngsymfonytools">Netgen Symfony Tools</a> extension',
-            'Version' => '1.0',
+            'Version' => '1.1',
             'Copyright' => 'Copyright (C) 2014 Netgen d.o.o.',
             'License' => 'GNU General Public License v2.0'
         );
